88657c63f018cfc135e084607b92da16484fa526
Lrop <Lagropner04@gmail.com>
2019-10-08 14:39:01 -0600

    herku deploy

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ $(document).ready(function (){
         $('.top-nav').toggleClass('open');
 
     });
+
+
+    $('.top-nav .nav-link').on('click', function (){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+
+
+
+
+    });
+
 });
 
-$('.nav-list').on('click', function(){
-    $(this).toggleClass('open');
-    $('.top-nav').toggleClass('open');
-    $('.open').collapse('hide');
-});
+// $('.nav-list').on('click', function(){
+//     $(this).toggleClass('open');
+//     $('.top-nav').toggleClass('open');
+//     $('.open').collapse('hide');
+// });
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('.materialboxed');
