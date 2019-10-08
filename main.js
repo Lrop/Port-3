ebdca639a0108ab1de086329892f838129b8e214
Lrop <Lagropner04@gmail.com>
2019-10-08 15:34:01 -0600

    npm

--- main.js
+++ main.js
@@ -15,17 +15,18 @@ $(document).ready(function (){
 
     });
 
-    $('nav a[href*="#"]').on('click', function (){
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
+    // $('nav a[href*="#"]').on('click', function (){
+    //     $('html, body').animate({
+    //         scrollTop: $($(this).attr('href')).offset().top - 100
 
-        } , 2000);
-    });
+    //     } , 2000);
+    // });
 
 
     AOS.init({
         easing: 'ease',
-        duration: 1800
+        duration: 1800,
+        once: true
 
     })
 });
