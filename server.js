bb0b94697ad03d38f1361f31188c2a67f3f741c1
Lrop <Lagropner04@gmail.com>
2019-10-15 13:31:13 -0600

    php

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+var http = require("http");
+
+var app = express();
+
+var app = express();
+app.get("/home.html", function(req, res) {
+  res.header("Content-Type", "text/html").send("<html>my html code</html>");
+});
+
+var PORT = process.env.PORT || 8080;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+
+// app.use(express.static("public"));
+// app.use(express.static(__dirname + '../public/Assets/styleFriend.css'));
+
+// require("./routing/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
+
+// response.sendfile("index.html");
+
+app.get("/", function(req, res) {
+  res.render("home", {});
+});
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
